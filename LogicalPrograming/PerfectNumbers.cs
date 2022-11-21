a0ccc1b77b0d54f6e23f6604515a82cd87c8df13
dericklopis <stanforever08@gmail.com>
2022-11-21 21:23:51 +0530

    Added: Perfect Number program

---
+++ LogicalPrograming/PerfectNumbers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograming
+{
+    internal class PerfectNumbers
+    {
+        
+        public static void PerfectNum()
+        {
+            int sum = 0, N;
+            Console.WriteLine("Enter the number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            N=num;
+            for(int i=1 ; i<num ; i++)
+            {
+                if ( num % i == 0)
+                {
+                    sum = sum+i;
+                }
+            }
+            if(sum==N)
+            {
+                Console.WriteLine("\n Entered number is a perfect number");
+            }
+            else
+            {
+                Console.WriteLine("\n Entered number is not perfect");
+            }
+            Console.ReadLine();
+        }
+    }        
+}
--- LogicalPrograming/Program.cs
+++ LogicalPrograming/Program.cs
@@ -11,12 +11,15 @@ namespace LogicalPrograming
         static void Main(string[] args)
         {
             Console.WriteLine("Please choose one program from below options");
-            Console.WriteLine("1:FibonacciSeries");
+            Console.WriteLine("1:FibonacciSeries \n2:PerfectNumbers");
             string options = Console.ReadLine();
             switch (options)
             {
                 case "1":
                     FibonacciSeries.Series();
+                    break;
+                case "2":
+                    PerfectNumbers.PerfectNum();
                     break;
             }
             Console.ReadLine();
